08530cc8844482fd06a22d5c97b567504fcfc318
Alex Kassil <alexkassil@sbcglobal.net>
2022-03-25 21:45:39 -0700

    move states to own class

---
+++ chapter2-state-machines/miner_owned_states.py
@@ -0,0 +1,17 @@
+from miner import Miner, State
+
+class EnterMineAndDigForNugget(State):
+    def execute(self, miner: Miner) -> None:
+        pass
+
+class VisitBankAndDepositGold(State):
+    def execute(self, miner: Miner) -> None:
+        pass
+
+class GoHomeAndSleepTilRested(State):
+    def execute(self, miner: Miner) -> None:
+        pass
+
+class QuenchThirst(State):
+    def execute(self, miner: Miner) -> None:
+        pass
