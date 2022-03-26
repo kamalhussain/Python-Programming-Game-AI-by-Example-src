be0ac33df19b367f3a5b145906c946611656dfd4
Alex Kassil <alexkassil@sbcglobal.net>
2022-03-25 21:45:26 -0700

    Add missing types

--- chapter2-state-machines/miner.py
+++ chapter2-state-machines/miner.py
@@ -1,44 +1,31 @@
 from __future__ import annotations
 from abc import ABC, abstractmethod
 from base_game_entity import BaseGameEntity
+from locations import LocationType
+from miner_owned_states import EnterMineAndDigForNugget, VisitBankAndDepositGold, GoHomeAndSleepTilRested, QuenchThirst
 
 class State(ABC):
     @abstractmethod
-    def execute(self, miner: Miner):
+    def execute(self, miner: Miner) -> None:
         pass
 
 class Miner(BaseGameEntity):
-    def __init__(self, id):
+    def __init__(self, id: int) -> None:
         super().__init__(id)
-        self.state = None
-        self.location_type = None
+        self.state: State = EnterMineAndDigForNugget()
+        self.location_type = LocationType.shack
         self.gold_carried = 0
         self.money_in_bank = 0
         self.thirst = 0
         self.fatigue = 0
 
-    def update(self):
+    def update(self) -> None:
         self.thirst += 1
         if self.state:
-            self.state.exectute(self)
+            self.state.execute(self)
 
-    def change_state(state):
+    def change_state(self, state: State) -> None:
         self.state = state
 
-class EnterMineAndDigForNugget(State):
-    def execute(self, miner: Miner):
-        pass
-
-class VisitBankAndDepositGold(State):
-    def execute(self, miner: Miner):
-        pass
-
-class GoHomeAndSleepTilRested(State):
-    def execute(self, miner: Miner):
-        pass
-
-class QuenchThirst(State):
-    def execute(self, miner: Miner):
-        pass
 
     
